0f0bd3e6311e9c9d4a00919285a9bf070f79d81d
Dara Adib <daradib@applemon.com>
2016-08-11 23:53:42 -0400

    Add django-mass-edit for bulk edits

--- applemonweb/settings.py
+++ applemonweb/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'formtools',
+    'massadmin',
     'armgmt',
 ]
 
--- applemonweb/urls.py
+++ applemonweb/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include('massadmin.urls')),
     url(r'^', include('armgmt.urls'), name='armgmt'),
 ]
