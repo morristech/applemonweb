2026e7723a058b6b87a38a0ea04626855640dd34
Dara Adib <daradib@applemon.com>
2016-08-12 17:28:20 -0400

    Create denormalized database views

---
+++ db_views.sql
@@ -0,0 +1,47 @@
+CREATE VIEW "invoices" AS
+SELECT client.name client_name,
+       substr(invoice.no, 0, 3) || '-' || substr(invoice.no, 3) `no`,
+       substr(project.no, 0, 3) || '-' || substr(project.no, 3) project_no,
+       invoice.date,
+       round(coalesce(amount, 0), 2) amount,
+       round(coalesce(old_amount, 0) + coalesce(paid, 0), 2) paid,
+       round(coalesce(amount, 0) - coalesce(old_amount, 0) - coalesce(paid, 0), 2) balance,
+       invoice.name,
+       project.name project_name
+FROM armgmt_invoice invoice
+LEFT JOIN armgmt_client client ON client.id = invoice.client_id
+LEFT JOIN armgmt_project project ON project.id = invoice.project_id
+LEFT JOIN
+  (SELECT invoice_id,
+          sum(qty * unit_price) amount
+   FROM armgmt_invoicelineitem
+   GROUP BY invoice_id) billed ON billed.invoice_id = invoice.id
+LEFT JOIN
+  (SELECT invoice_id,
+          sum(qty * unit_price) old_amount
+   FROM armgmt_invoicelineitem l
+   INNER JOIN armgmt_invoice i ON i.id = l.invoice_id
+   WHERE i.date < '2012-09-01'
+   GROUP BY invoice_id) old_billed ON old_billed.invoice_id = invoice.id
+LEFT JOIN
+  (SELECT invoice_id,
+          sum(amount) paid
+   FROM armgmt_payment
+   GROUP BY invoice_id) paid ON paid.invoice_id = invoice.id
+ORDER BY `no` ASC
+
+CREATE VIEW "payments" AS
+SELECT payment.date,
+       substr(invoice.no, 0, 3) || '-' || substr(invoice.no, 3) invoice_no,
+       payment.amount
+FROM armgmt_payment payment
+LEFT JOIN armgmt_invoice invoice ON invoice.id = payment.invoice_id
+UNION
+SELECT invoice.date,
+       substr(invoice.no, 0, 3) || '-' || substr(invoice.no, 3) invoice_no,
+       round(sum(qty * unit_price), 2) old_amount
+FROM armgmt_invoicelineitem l
+INNER JOIN armgmt_invoice invoice ON invoice.id = l.invoice_id
+WHERE invoice.date < '2012-09-01'
+GROUP BY invoice_id
+ORDER BY payment.date ASC
