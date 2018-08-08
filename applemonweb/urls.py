1178c2dcf675ec1e5cbea3f6b3fcb4c1f7a9996f
Dara Adib <daradib@applemon.com>
2018-08-08 17:58:03 -0400

    More Sentry integration

--- applemonweb/urls.py
+++ applemonweb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseServerError
+from django.template import loader
 from django.urls import path
 
 
@@ -10,3 +12,13 @@ urlpatterns = [
     url(r'^', include('armgmt.urls'), name='armgmt'),
     url(r'^', include('datalogger.urls'), name='datalogger'),
 ]
+
+
+def handler500(request):
+    """500 error handler which includes ``request`` in the context."""
+    try:
+        template = loader.get_template('500.html')
+        body = template.render(request=request)
+        return HttpResponseServerError(body)
+    except Exception:
+        return HttpResponseServerError('500 Internal Server Error')
--- applemonweb/wsgi.py
+++ applemonweb/wsgi.py
@@ -1,5 +1,8 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applemonweb.settings')
-application = get_wsgi_application()
+application = Sentry(get_wsgi_application())
